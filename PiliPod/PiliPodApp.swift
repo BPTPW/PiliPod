0dfe01c88628eeac986ce9aea770e61147b477af
CO <bptpw_service@163.com>
2026-05-21 13:54:57 +0800

    Initial Commit

---
+++ PiliPod/PiliPodApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiliPodApp.swift
+//  PiliPod
+//
+//  Created by co on 2026/5/21.
+//
+
+import SwiftUI
+
+@main
+struct PiliPodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PiliPodTests/PiliPodTests.swift
@@ -0,0 +1,18 @@
+//
+//  PiliPodTests.swift
+//  PiliPodTests
+//
+//  Created by co on 2026/5/21.
+//
+
+import Testing
+
+struct PiliPodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
